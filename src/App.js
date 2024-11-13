7f4e9061f8b5f29205cbbeb23acb433b50ad6d1a
leela2706 <saileelavardhan12@gmail.com>
2024-11-13 10:27:20 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import HeaderTop from './Components/HeaderTop';
+import Hero from './Components/Hero';
+import ListTopics from './Components/ListTopics';
+import Works from './Components/Works';
+import Explore from './Components/Explore';
+import Reviews from './Components/Reviews';
+import Statistics from './Components/Statistics';
+import Blog from './Components/Blog';
+import Subscription from './Components/Subscription';
+import Footer from './Components/Footer';
+import TopArea from './Components/TopArea';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome</h2>
+      {/* <HeaderTop /> */}
+
+      {/* <ListTopics /> */}
+
+
+
+      {/* <Statistics /> */}
+      {/* <Blog /> */}
+
+      {/* <Footer /> */}
+              <TopArea/>   
+
+      <Routes>
+
+        <Route path="/home" element={<Hero />} />
+        <Route path="/HOWITWORKS" element={<Works />} />
+        <Route path="/EXPLORE" element={<Explore />} />
+        <Route path="/REVIEW" element={<Reviews />} />
+        <Route path="/CONTACT" element={<Subscription />} />
+      </Routes>
     </div>
   );
 }
